cb1733060cd11dbe1b29997c92ccde983d118570
VerySneaky77 <carlquin77@gmail.com>
2019-06-20 16:43:47 -0700

    Corrected structure of database to test routing, admin .get routing working

---
+++ scripts/seedDB.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+//Load User model
+const Item = require("../models/menuItem");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/restaurant");
+
+const itemsSeed = [
+  {
+    name: "Burger",
+    ingredients: "sesame seed bun, beef patty",
+    description: "A classic sandwich favorite, customized to order.",
+    price : 8.0
+  }
+];
+
+Item.remove({})
+  .then(() => Item.collection.insertMany(itemsSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
