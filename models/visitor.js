ca90088268ed2fc839c1d8e9c1060d2310e690e6
VerySneaky77 <carlquin77@gmail.com>
2019-06-21 15:33:11 -0700

    Added order get and post route

--- models/visitor.js
+++ models/visitor.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const visitorSchema = new Schema({
-    nameLast: String,
-    nameFirst: String,
-    phone: { type: Number, required: true },
-    orders: []
+  nameLast: String,
+  nameFirst: String,
+  phone: { type: Number, required: true }
 });
 
 const Visitors = mongoose.model("visitor", visitorSchema);
