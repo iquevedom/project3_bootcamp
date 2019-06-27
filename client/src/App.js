f6ceac228411e6febf3b7a6b273ee49e85f39c3a
bryce <blrippey@gmail.com>
2019-06-27 13:38:07 -0700

    sdnf

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/NavSection";
 import './App.css';
 
 // TK
--- client/src/components/pages/Home/AdminHome.js
+++ client/src/components/pages/Home/AdminHome.js
@@ -1,19 +1,22 @@
-import React from "react";
-import {Container, Row, Col, Navbar} from "react-bootstrap";
+import React, {Component} from "react";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 
-function AdminHome(props) {
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Title>Welcome admin!</Title>
-          <p style={{ maxWidth: 600, margin: "auto" }}>
-            This is the main page for management of the bussiness.
+class AdminHome extends Component{
+
+  render(){
+    return (
+      <div>
+        <Container>
+          <Row>
+            <h1>hello</h1>
+            <p style={{ maxWidth: 600, margin: "auto" }}>
+              This is the main page for management of the bussiness.
           </p>
-        </Row>
-      </Container>
-    </div>
-  );
+          </Row>
+        </Container>
+      </div>
+    );
+  };
 }
 
 export default AdminHome;
